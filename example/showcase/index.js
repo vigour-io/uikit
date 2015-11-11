5f896f86fbf7163382c6127fdd8be0975817aa60
Youri Daamen <me@youz.io>
2015-11-11 21:50:27 +0100

    found some weirdness

--- example/showcase/index.js
+++ example/showcase/index.js
@@ -6,90 +6,95 @@ Observable.prototype.inject(require('vigour-js/lib/operator/add'))
 Observable.prototype.inject(require('vigour-js/lib/operator/subscribe'))
 Observable.prototype.inject(require('vigour-js/lib/operator/transform'))
 
-
+Element.prototype.inject(require('vigour-element/lib/property/style'))
 
 require('./style.less')
 
 var data = new Observable({
-  listitems: {
-    items: [{
-      title: 'one thing',
-      subtitle: 'subsubsubsubs11'
-    }, {
-      title: 'two thing',
-      subtitle: 'subsubsubsubs22'
-    }, {
-      title: 'three thing',
-      subtitle: 'subsubsubsubs33'
-    }, {
-      title: 'four thing',
-      subtitle: 'subsubsubsubs44'
-    }, {
-      title: 'five thing',
-      subtitle: 'subsubsubsubs55'
-    }]
-  },
-  listitems2: {
-    items: [
-      'one thing2222222222',
-      'two thing2222222222',
-      'three thing2222222222',
-      'four thing2222222222',
-      'five thing2222222222'
-    ]
-  },
-  defaultText: 'The'
+  list: [{
+    title: 'one thing',
+    subtitle: 'subsubsubsubs11'
+  }, {
+    title: 'two thing',
+    subtitle: 'subsubsubsubs22'
+  }, {
+    title: 'three thing',
+    subtitle: 'subsubsubsubs33'
+  }, {
+    title: 'four thing',
+    subtitle: 'subsubsubsubs44'
+  }, {
+    title: 'five thing',
+    subtitle: 'subsubsubsubs55'
+  }]
+})
+
+
+var videoData = new Observable({
+  list: [{
+    title: 'video 1',
+    subtitle: 'subsubsubsubs11'
+  }, {
+    title: 'video 2',
+    subtitle: 'subsubsubsubs22'
+  }, {
+    title: 'video 3',
+    subtitle: 'subsubsubsubs33'
+  }, {
+    title: 'video 4',
+    subtitle: 'subsubsubsubs44'
+  }, {
+    title: 'video 5',
+    subtitle: 'subsubsubsubs55'
+  }]
 })
 
 window.data = data
 
 var app = new Element({
-  val: data,
   node: document.body
 })
 
 // setInterval(function () {
-//   data.listitems.items.each(function(prop){
+//   data.list.each(function(prop){
 //     prop.remove()
 //     return true
 //   })
-//   data.listitems2.items.set({
+//   data.list2.set({
 //     [Math.random()]: Math.random()
 //   })
 // }, 2000)
 
 var Input = require('~/lib').Input
 
+var Block = new Element({
+  titlefield: new Input({
+    $: 'title'
+  }),
+  subtitlefield: new Input({
+    $: 'subtitle'
+  })
+}).Constructor
+
+var List = new Element({
+  ChildConstructor: Block,
+  $: 'list'
+}).Constructor
+
 app.set({
+  // val: data,
   header: require('./components/header'),
-  container: {
-    css: 'main',
-    ChildConstructor: new Element({
-      titlefield: new Input({
-        $: 'title'
-      }),
-      subtitlefield: new Input({
-        $:'subtitle'
-      })
-    }).Constructor,
-    $: 'listitems.items'
-  },
-  things:{
-    ChildConstructor:new Element({
-      text:{
-        $:'title.upward.listitems.items.0.title'
-      }
-    }).Constructor,
-    $:'listitems.items'
-  }
+  container:new List(data),
+  things:new List(data),
+  specialVideoSection:new List(videoData)
 })
 
 
 window.app = app
 
 // console.log('=====>', app.container._cache[0])
-// data.listitems.items[1].title.val = 'smurk'
+// data.list[1].title.val = 'smurk'
 
 // app.container._cache[0].set({
-//   $subscribe:data.listitems.items[0].title
+//   $subscribe:data.list[0].title
 //   })
