fede9df31de0b72fb4a0f4b5924265764062795d
Youri Daamen <me@youz.io>
2015-11-14 15:12:50 +0100

    example collection

--- example/list/index.js
+++ example/list/index.js
@@ -1,5 +1,5 @@
 'use strict'
-require('vigour-scratch/index.less')
+// require('vigour-scratch/index.less')
 require('./style.less')
 
 var Element = require('vigour-element')
@@ -7,18 +7,19 @@ var Observable = require('vigour-js/lib/observable')
 var contentdata = new Observable(require('./data.json'))
 
 Element.prototype.inject(require('vigour-element/lib/property/text'))
-// Element.prototype.inject(require('vigour-element/lib/events'))
 Observable.prototype.inject(require('vigour-js/lib/operator/subscribe'))
 
-var TextItem = new Element({
-  titlefield:{
-    text:{
-      $:'title'
+var TextElement = new Element({
+  titlefield: {
+    text: {
+      $: 'title'
     }
   },
-  info:{
-    text:{
-      $:'description'
+  info: {
+    nested: {
+      text: {
+        $: 'description'
+      }
     }
   }
 }).Constructor
@@ -27,94 +28,31 @@ var Input = new Element({
   node: 'input',
   $: 'title',
   on: {
-    data () {
-      var node = this.node
-      var value = this.val
-      if (node.value !== value) {
-        node.value = value
+    data() {
+        var node = this.node
+        var value = this.val
+        if (node.value !== value) {
+          node.value = value
+        }
+      },
+      keyup() {
+        this.$subscribe.origin.val = this.node.value
       }
-    },
-    keyup () {
-      this.$subscribe.origin.val = this.node.value
-    }
   }
 }).Constructor
 
 var app = new Element({
-  val:contentdata,
-  node:document.body,
-  topbar:{
-    text:'TOPBAR!'
+  val: contentdata,
+  node: document.body,
+  topbar: {
+    text: 'topbar?!'
   },
-  inputlist:{
-    ChildConstructor:Input,
-    $:'shows'
+  inputlist: {
+    ChildConstructor: Input,
+    $: 'shows'
   },
-  container:{
-    ChildConstructor:TextItem,
-    $:'shows'
-  },
+  container: {
+    ChildConstructor: TextElement,
+    $: 'shows'
+  }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var InputTitle = new Element({
-//   node: 'input',
-//   $: 'title',
-//   on: {
-//     data () {
-//       var node = this.node
-//       var value = this.val
-//       if (node.value !== value) {
-//         node.value = value
-//       }
-//     },
-//     keyup () {
-//       this.$subscribe.origin.val = this.node.value
-//     }
-//   }
-// }).Constructor
-
-// var InputId = new InputTitle({
-//   $: 'id'
-// }).Constructor
-
-// var app = new Element({
-//   val: contentdata,
-//   node: document.body,
-//   topbar: {
-//     node: 'header',
-//     text: 'this is a topbar'
-//   },
-//   container: {
-//     ChildConstructor: new Element({
-//       titlefield: new InputTitle(),
-//       secondTitle: new InputId(),
-//       info: {
-//         text: {
-//           $: 'description'
-//         }
-//       }
-//     }).Constructor,
-//     $: 'shows'
-//   }
-// })
