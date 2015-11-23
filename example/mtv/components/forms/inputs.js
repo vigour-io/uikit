d37afb0266473b413766b780a856324879da6c14
nikoloza <nika.tomadze@gmail.com>
2015-11-23 18:58:42 +0100

    binding example

--- example/mtv/components/forms/inputs.js
+++ example/mtv/components/forms/inputs.js
@@ -3,15 +3,39 @@ require('./style.less')
 var Element = require('vigour-element')
 var ui = require('~/lib')
 
+var Observable = require('vigour-js/lib/observable')
+
+Element.prototype.inject(
+  require('vigour-js/lib/operator/subscribe')
+)
+
+var obs = window.obs = new Observable('asd')
+
 module.exports = new Element({
   title: new ui.H5('Inputs:'),
   node: 'section',
 
   sizesCaption: new ui.P('Input sizes:'),
-  'ui-large': new ui.Input(),
-  'ui-big': new ui.Input(),
-  'ui-medium': new ui.Input(),
-  'ui-small': new ui.Input(),
+  'ui-large': new ui.Input({
+    text: obs
+  }),
+  'ui-big': new ui.Input({
+    text: obs
+  }),
+  'ui-medium': new ui.Input({
+    text: obs
+  }),
+  'ui-small': new ui.Input({
+    text: obs
+  }),
+
+  aside: new ui.P({
+    css: {
+      addClass: 'ui-primary'
+    },
+    prepend: 'asd',
+    text: obs
+  }),
 
   colorCaption: new ui.P('Input colors:'),
   'ui-primary': new ui.Input(),
